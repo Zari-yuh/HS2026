4dcc1e4ae63e8c69c7f6973c785e0907e313c123
Zarii . <zarii.@Zariis-MacBook-Pro.local>
2026-06-16 13:39:27 -0700

    Add Python Profile varibles lab

---
+++ profiles.py
@@ -0,0 +1,19 @@
+full_name = "Zari Asiyah Weeks"
+nickname = "Asi"
+age = 18
+
+use_python_before = False
+
+hobbies = ["drawing", "sleeping", "watching anime"]
+
+favorites = { "movie": "Pirates of the Caribbean",
+             "food": "french toast",
+             "hobby": "drawing",
+             "activity": "sleeping"}
+
+print (full_name, nickname, age, use_python_before, hobbies, favorites)
+
+skills = ("gaming", "science", "teamwork")#tuple: ordered, cannot change
+languages_spoken = ("english")# set: no duplicates
+
+print (skills,languages_spoken)
